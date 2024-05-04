f2165f712177aa10790faebf8f53a1af224e58a3
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-05-04 20:57:08 +0100

    starting the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className="game"></div>
+      </main>
+    </>
+  )
+}
+
+export default App
